3bb8c28d448ce446955c7b3d12702198ae312e32
Luis Pichardo <106943934+luisedpichardo@users.noreply.github.com>
2025-12-20 16:03:44 -0500

    Permissions for location

---
+++ src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+
+export const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import Geolocation from '@react-native-community/geolocation';
+import MapView from 'react-native-maps';
 // Types
 import { NavHomeTab, NavRoot } from '../navigation/types';
+import { useUserStore } from '../stores/userStore';
+import { Loading } from '../components/Loading';
 
 type Props = CompositeScreenProps<
   BottomTabScreenProps<NavHomeTab, 'Map'>,
@@ -11,10 +16,46 @@ type Props = CompositeScreenProps<
 >;
 
 export const Main = ({ navigation }: Props) => {
+  const user = useUserStore(state => state.user);
+  const [currPosition, setCurrPosition] = useState({ lat: 0, long: 0 });
+  const [loading, setLoading] = useState(true);
+  const mapRef = useRef<MapView | null>(null);
+
+  // Set to load current positon
+  useEffect(() => {
+    const watchId = watchLocation();
+    return () => Geolocation.clearWatch(watchId);
+  }, []);
+
+  // Watch current position
+  const watchLocation = () => {
+    const watchID = Geolocation.watchPosition(
+      async (data: any) => {
+        // const loc: Location = {
+        const loc: any = {
+          lat: data.coords.latitude,
+          long: data.coords.longitude,
+          email: user.email,
+          timestamp: data.timestamp,
+        };
+        setCurrPosition({
+          lat: data.coords.latitude,
+          long: data.coords.longitude,
+        });
+        console.log(loc);
+        // await setMyLocationService(loc);
+        setLoading(false);
+      },
+      (err: any) => {
+        Alert.alert(err.message);
+      },
+    );
+    return watchID;
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.txt}>Map</Text>
+      {loading ? <Loading /> : <Text style={styles.txt}>Map</Text>}
     </View>
   );
 };
