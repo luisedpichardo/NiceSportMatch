11f3f5c54c9698886191a2fbab4513bb3cd0462e
Luis Pichardo <106943934+luisedpichardo@users.noreply.github.com>
2026-01-01 19:18:14 -0500

    missing translation

--- src/components/OpenSettings.tsx
+++ src/components/OpenSettings.tsx
@@ -1,15 +1,17 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useTranslation } from 'react-i18next';
 // Components
 import { RightHdrBtn } from './RightHdrBtn';
 // Navigation
 import { NavRoot } from '../navigation/types';
 
 export const OpenSettings = () => {
+  const { t } = useTranslation();
   const navigation = useNavigation<NativeStackNavigationProp<NavRoot>>();
   return (
     <RightHdrBtn
-      text="Settings"
+      text={t('home-tabs.open-settings')}
       color="black"
       onPress={() => navigation.navigate('Settings')}
     />
