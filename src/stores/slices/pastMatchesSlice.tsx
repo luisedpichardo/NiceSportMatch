411d34a609d540351c35352d999d16bf7b6cb9f2
Luis Pichardo <106943934+luisedpichardo@users.noreply.github.com>
2026-01-06 15:19:29 -0500

    slices for local state management redux

---
+++ src/stores/slices/pastMatchesSlice.tsx
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type address = {
+  lat: number;
+  long: number;
+};
+
+type Match = {
+  _id: string;
+  address: address;
+  day: string;
+  publisher: string;
+  status: string;
+  time: string;
+};
+
+const initialState: Match[] = [];
+
+const pastMatchesSlice = createSlice({
+  name: 'past matches',
+  initialState,
+  reducers: {
+    onAddMatch: (state, action) => {
+      return [
+        ...state,
+        {
+          _id: action.payload._id,
+          address: action.payload.address,
+          day: action.payload.day,
+          publisher: action.payload.publisher,
+          status: action.payload.status,
+          time: action.payload.time,
+        },
+      ];
+    },
+    onRemoveMatch: (state, action) => {
+      return state.filter((match: any) => match.match !== action.payload.match);
+    },
+  },
+});
+
+export default pastMatchesSlice.reducer;
