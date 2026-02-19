d3c4c40a93420040090632a235b5a57436a772fa
Luis Pichardo <106943934+luisedpichardo@users.noreply.github.com>
2026-02-19 09:10:51 -0500

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|zustand)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|zustand|@react-native-firebase)/)',
+    // 'node_modules/(?!(jest-)?react-native|@react-native(-community)?|react-redux|@reduxjs|immer|@react-native-firebase|@react-navigation|toastify-react-native|expo(nent)?|@expo(nent)?/.*)',
   ],
 };
